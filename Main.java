f5e2ecf1d058ed04001b8c38ab4368e31646ccdb
Ryan James <ryanjames1729@gmail.com>
2025-04-01 12:49:50 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 // Selection Starter      A+CR
 import java.util.Arrays;
+import java.util.ArrayList;
 
 class Main {
   public static void main(String[] args) {
@@ -14,5 +15,17 @@ class Main {
       System.out.println(Arrays.toString(ray1));
       System.out.println(SelSorter.sel_sort_Entry(ray1));
       System.out.println(Arrays.toString(ray1)); 
+
+      // NEW CODE BELOW
+      ArrayList<Integer> myList = new ArrayList<Integer>();
+      for(int i = 0; i < 10; i++){
+        myList.add((int)(Math.random()*100));
+      }
+      System.out.println("\n\nUnsorted List: " + myList);
+      // Write your algorithm
+
+
+      
+
   }
 }
